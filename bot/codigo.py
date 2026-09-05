2c7c5ed6816cd60577c2f3a63a9008900fdd2b86
diegolazzari79 <61602695+diegolazzari79@users.noreply.github.com>
2026-09-05 07:16:27 -0300

    Create codigo.py

---
+++ bot/codigo.py
@@ -0,0 +1,130 @@
+import pyautogui
+import time
+
+for numero in range(200):
+    pyautogui.PAUSE = 2
+    pyautogui.press("win")
+    pyautogui.write("chrome")
+    pyautogui.press("enter") #abre chrome
+    time.sleep(5)
+    pyautogui.click(x=500, y=452) #clica perfil
+    pyautogui.scroll(5000)
+    time.sleep(5)
+
+    pyautogui.click(x=286, y=450) #clica botão frota
+    time.sleep(5)
+
+    pyautogui.click(x=1018, y=276) #y=305) #clica planeta 1.40
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(7)
+
+    pyautogui.click(x=1018, y=326) #y=355) #clica planeta 1.151
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(8)
+
+    pyautogui.click(x=1018, y=373) #y=402) #clica planeta 1.245
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(6)
+
+    pyautogui.click(x=1018, y=450) #clica planeta 1.340
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(8)
+
+    pyautogui.click(x=1018, y=421) #y=499) #clica planeta 2.175
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(7)
+
+    pyautogui.click(x=1018, y=545) #clica planeta 2.412
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(7)
+
+    pyautogui.click(x=1018, y=516) #y=593) #clica planeta 3.67
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    #pyautogui.click(x=1112, y=593) #clica lua 3.67
+    #time.sleep(5)
+    #pyautogui.click(x=886, y=330) #clica missão expo
+    #pyautogui.click(x=886, y=330) #clica missão
+    #time.sleep(1)
+    #pyautogui.press("enter")
+    #time.sleep(7)
+    #pyautogui.click(x=1112, y=593) #clica lua 3.67
+    #time.sleep(5)
+    #pyautogui.click(x=886, y=330) #clica missão expo
+    #pyautogui.click(x=886, y=330) #clica missão
+    #time.sleep(1)
+    #pyautogui.press("enter")
+    #time.sleep(6)
+    #pyautogui.click(x=1112, y=593) #clica lua 3.67
+    #time.sleep(5)
+    #pyautogui.click(x=886, y=330) #clica missão expo
+    #pyautogui.click(x=886, y=330) #clica missão
+    #time.sleep(1)
+    #pyautogui.press("enter")
+    #time.sleep(5)
+
+    pyautogui.click(x=1018, y=613) #y=642) #clica planeta 3.209
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(6)
+
+    pyautogui.click(x=1018, y=662) #y=691) #clica planeta 3.368
+    time.sleep(5)
+    pyautogui.click(x=886, y=301) #clica missão expo    
+    pyautogui.click(x=886, y=301) #clica missão
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(7)
+
+
+    #pyautogui.click(x=1018, y=735) #clica planeta 4.92
+    #time.sleep(5)
+    #pyautogui.click(x=886, y=330) #clica missão expo
+    #pyautogui.click(x=886, y=330) #clica missão
+    #time.sleep(1)
+    #pyautogui.press("enter")
+    #time.sleep(7)
+
+    pyautogui.click(x=1249, y=18) #clica minimizar navegador
+    time.sleep(4444)
+
+
+
+
+
+
+
+
+
